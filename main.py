ae6399fe1ffecd293bbe835bd5563de9ee546263
Andrew Ochepovsky <ochepovsky@mail.ru>
2023-03-01 21:28:14 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from IPython.display import display
 import pandas as pd
 import openpyxl as xl
 path = r'source\\test.xlsx'
@@ -6,7 +7,7 @@ n_sheets = len(wb.worksheets)
 print(n_sheets)
 
 # !!! After using wb.worksheets return kast sheet
-sheet1 = wb.active
+# sheet1 = wb.active
 sheet1 = wb.worksheets[0]
 print(sheet1)
 sheet2 = wb.worksheets[1]
@@ -23,16 +24,45 @@ for i in range(1, max_row+1):
         print(cell.value, end=' ')
     print()
 
-
+# read xlsx (not xls!!!) file into Pandas.Dataframe
+print('-------info--------------')
 df = pd.read_excel(path, engine='openpyxl')
-print(df.info)
+display(df.info)
+print('--------display(df)-------------')
+display(df)
+print('--------info()-------------')
+print(df.info())
+print('-------describe()--------------')
+print(df.describe())
+print('-------shape--------------')
+print(df.shape)
+print('-------columns()--------------')
+print(df.columns)
 
-# pip install openpyxl-stubs
+index = df.index
+print(index)
 
-'''import xlwings as xw
-# connect to book
-wb = xw.Book(r'source\test.xlsx') # !!! use raw string - see first letter 'r'
+print(df['A'])
 
-#work with sheets
-num_sheets = wb.sheets.count
-print(num_sheets)'''
+sA = df['A']
+print(type(sA))
+
+val = sA[2]
+print(type(val))
+print(val)
+
+# get cell value via index
+print(df.iloc [2] ['B'])
+print(df.at[1, 'C'])
+
+# get a specific row
+display(df.iloc[2])
+
+# Pandas Get the Number of Rows
+display(len(df.index)) # 1
+display(df.shape[0])   # 2
+display(len(df.axes[0])) # 3
+
+# Pandas Get the Number of Columns
+display(df.shape[1])  # 1
+display(len(df.axes[1])) # 2
